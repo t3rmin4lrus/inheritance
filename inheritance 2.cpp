cd10ed3836d18dbf53a46382f4bc5bf390939316
t3rmin4lrus <87004569+t3rmin4lrus@users.noreply.github.com>
2021-11-28 15:07:00 +0300

    Add files via upload
    Информация для преподавателя: смотрите задание #1-2 в файлах с расширением .cpp. Разбил так, поскольку программа при исполнении отображает только решение 1-ой программы. Проработку элементов для задания #3  смотрите в файле с расширением .docx.

---
+++ inheritance 2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+
+//Задание 2: класс Фрукты:
+
+class Fruit
+{
+private:
+    std::string n_name;
+    std::string n_color;
+
+public:
+    Fruit(std::string name, std::string color) : n_name(name), n_color(color) {}
+    std::string getName() { return n_name; }
+    std::string getColor() { return n_color; }
+};
+
+class Apple:public Fruit
+{
+protected:
+    Apple(std::string name, std::string color) : Fruit(name, color) {}
+
+public:
+    Apple(std::string color = "Red") : Fruit("apple", color) {}
+};
+
+class GrannySmith:public Apple
+{
+public:
+    GrannySmith():Apple("Granny Smith apple", "green") {}
+};
+
+class Banana:public Fruit
+{
+public:
+    Banana(std::string name = "Banana", std::string color = "Yellow") : Fruit(name, color) {}
+};
+
+int main()
+{
+    Apple a("red");
+    Banana b;
+    GrannySmith c;
+
+    std::cout << "My " << a.getName() << " is " << a.getColor() << ".\n";
+    std::cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
+    std::cout << "My " << c.getName() << " is " << c.getColor() << ".\n";
+
+    return 0;
+}
---
+++ inheritance.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <string>
+
+//Задание 1: класс Персоны:
+
+class Person
+{
+private:
+    static int count;
+    std::string m_name;
+    int m_age;
+    std::string m_gender;
+    int m_weight;
+
+
+public:
+    static int GetCount() { return count; }
+
+    Person(const std::string& name, const int& age, const std::string& gender, const int& weight)
+        : m_name(name), m_age(age), m_weight(weight), m_gender(gender)
+    {
+        count++;
+    }
+
+    std::string GetName() { return m_name; }
+    int GetAge() { return m_age; }
+    int GetWeight() { return m_weight; }
+    virtual void Print()  
+    {
+        std::cout << m_name << ' ' << m_age << ' ' << m_gender << ' ' << m_weight <<std::endl;
+    }
+};
+
+class Student : public Person
+{
+private:
+    int m_year;
+
+public:
+    Student(int year, const std::string& name, const int age, const std::string& gender, const int weight)
+        :Person(name, age, gender, weight), m_year(year)
+    {}
+    int GetYear() { return m_year; }
+    void AddYear(int addValue)
+    {
+        m_year += addValue;
+    }
+    void AddYear()
+    {
+        m_year++;
+    }
+
+    void Print()
+    {
+        std::cout << m_year << ' ';
+        Person::Print();
+    }
+};
+
+int Person::count = 0;
+
+int main()
+{
+    const int N = 2;
+    Person* persona[N];
+    for (int i = 0; i < N; i++)
+    {
+        int year, weight, age;
+        std::string name, gender;
+        std::cout << "Enter name, age, gender, weight:" << std::endl;
+        std::cin >> name >> age >> gender >> weight >> year;
+        persona[i] = new Student(year, name, age, gender, weight);
+        std::cout << "Count=" << persona[i]->GetCount() << std::endl;
+    }
+    for (int i = 0; i < N; i++)
+        persona[i]->Print();
+    Student* s1 = static_cast<Student*>(persona[0]);
+    s1->AddYear(2);
+    std::cout << "New age of " << s1->GetName() << " = "
+        << s1->GetAge() << std::endl;
+    for (int i = 0; i < N; i++)
+        delete persona[i];
+    return 0;
+}
